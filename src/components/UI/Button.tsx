3d030b352eacd204e80d1e90b052b70cc8c9359e
halderjoydeep <42032921+halderjoydeep@users.noreply.github.com>
2024-04-25 23:51:42 +0530

    add cva

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -6,17 +6,17 @@ interface ButtonProps extends React.ComponentProps<"button"> {
 }
 
 const Button = ({ white, className, children }: ButtonProps) => {
+  const buttonClass = cn(
+    "button relative inline-flex h-11 items-center justify-center px-7 transition-colors hover:text-color-1",
+    {
+      "text-n-8": white,
+      "text-n-1": !white,
+    },
+    className,
+  );
+
   return (
-    <button
-      className={cn(
-        "button relative inline-flex h-11 items-center justify-center px-7 transition-colors hover:text-color-1",
-        {
-          "text-n-8": white,
-          "text-n-1": !white,
-        },
-        className,
-      )}
-    >
+    <button className={buttonClass}>
       <span className="z-2">{children}</span>
       <ButtonSvg white={white} />
     </button>
