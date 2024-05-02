09eec3f0d5861abe029ea9491148b3676ec1f927
halderjoydeep <42032921+halderjoydeep@users.noreply.github.com>
2024-05-02 23:12:08 +0530

    Implementing next/font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,28 @@
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
-// import { Sora, Source_Code_Pro, Space_Grotesk } from "next/font/google";
+import { Sora, Source_Code_Pro, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 
-// const sora = Sora({
-//   subsets: ["latin"],
-//   weight: ["300", "400", "600"],
-//   display: "swap",
-//   variable: "--font-sora",
-// });
+const sora = Sora({
+  subsets: ["latin"],
+  weight: ["300", "400", "600"],
+  display: "swap",
+  variable: "--font-sora",
+});
 
-// const spaceGrotesk = Space_Grotesk({
-//   subsets: ["latin"],
-//   weight: ["300"],
-//   display: "swap",
-//   variable: "--font-grotesk",
-// });
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300"],
+  display: "swap",
+  variable: "--font-grotesk",
+});
 
-// const sourceCodePro = Source_Code_Pro({
-//   subsets: ["latin"],
-//   weight: ["400", "600", "700"],
-//   display: "swap",
-//   variable: "--font-code",
-// });
+const sourceCodePro = Source_Code_Pro({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+  display: "swap",
+  variable: "--font-code",
+});
 
 export const metadata: Metadata = {
   title: "Brainwave",
@@ -39,9 +39,9 @@ export default function RootLayout({
       <body
         className={cn(
           "min-h-screen scroll-smooth bg-n-8 font-sans text-n-1 antialiased",
-          // sora.variable,
-          // sourceCodePro.variable,
-          // spaceGrotesk.variable,
+          sora.variable,
+          sourceCodePro.variable,
+          spaceGrotesk.variable,
         )}
       >
         {children}
