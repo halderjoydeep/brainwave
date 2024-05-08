8270fad86a5cdf30243845e6a8522137ad40b96d
halderjoydeep <42032921+halderjoydeep@users.noreply.github.com>
2024-05-08 18:59:23 +0530

    Implement Canvas drop shadow

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -45,39 +45,37 @@ export default function Hero() {
         <div className="relative mx-auto max-w-[23rem] md:max-w-5xl xl:mb-24">
           {/* Canvas */}
           <div className="relative z-1 rounded-2xl bg-conic-gradient p-0.5">
-            {/* Inner canvas */}
-            <div className="relative rounded-2xl bg-n-8">
-              {/* Top gray bar */}
-              <div className="h-[1.4rem] rounded-t-[0.9rem] bg-n-10" />
-
-              {/* Image Canvas */}
-              <div className="aspect-[33/40] overflow-hidden rounded-b-[0.9rem] md:aspect-[688/490] lg:aspect-[1024/490]">
-                <Image
-                  src={robot}
-                  className="w-full translate-y-[8%] scale-[1.7] md:-translate-y-[10%] md:scale-[1] lg:-translate-y-[23%]"
-                  alt="AI"
-                />
-
-                <Generating className="absolute bottom-5 left-4 right-4 md:bottom-8 md:left-1/2 md:right-auto md:w-[31rem] md:-translate-x-1/2" />
-
-                <ScrollParallax isAbsolutelyPositioned>
-                  <ul className="absolute -left-[5.5rem] bottom-[7.5rem] hidden items-center rounded-2xl border border-n-1/10 bg-n-9/40 p-1 backdrop-blur xl:flex">
-                    {heroIcons.map((heroIcon, index) => (
-                      <li key={index} className="p-5">
-                        <Image src={heroIcon} alt="icons" />
-                      </li>
-                    ))}
-                  </ul>
-                </ScrollParallax>
-
-                <ScrollParallax isAbsolutelyPositioned>
-                  <Notification />
-                </ScrollParallax>
-              </div>
+            {/* Top gray bar */}
+            <div className="h-[1.4rem] rounded-t-[0.9rem] bg-n-10" />
+            {/* Image Canvas */}
+            <div className="aspect-[33/40] overflow-hidden rounded-b-[0.9rem] md:aspect-[688/490] lg:aspect-[1024/490]">
+              <Image
+                src={robot}
+                className="w-full translate-y-[8%] scale-[1.7] md:-translate-y-[10%] md:scale-[1] lg:-translate-y-[23%]"
+                alt="AI"
+              />
             </div>
 
-            {/* Gradient */}
+            <Generating className="absolute bottom-5 left-4 right-4 md:bottom-8 md:left-1/2 md:right-auto md:w-[31rem] md:-translate-x-1/2" />
+
+            <ScrollParallax isAbsolutelyPositioned>
+              <ul className="absolute -left-[5.5rem] bottom-[7.5rem] hidden items-center rounded-2xl border border-n-1/10 bg-n-9/40 p-1 backdrop-blur xl:flex">
+                {heroIcons.map((heroIcon, index) => (
+                  <li key={index} className="p-5">
+                    <Image src={heroIcon} alt="icons" />
+                  </li>
+                ))}
+              </ul>
+            </ScrollParallax>
+
+            <ScrollParallax isAbsolutelyPositioned>
+              <Notification />
+            </ScrollParallax>
           </div>
+
+          {/* Canvas drop shadows */}
+          <div className="relative z-1 mx-2.5 h-6 rounded-b-[1.25rem] bg-n-11 shadow-xl lg:mx-8" />
+          <div className="relative z-1 mx-6 h-6 rounded-b-[1.25rem] bg-n-11/70 shadow-xl lg:mx-20" />
 
           {/* Hero Background */}
           <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
